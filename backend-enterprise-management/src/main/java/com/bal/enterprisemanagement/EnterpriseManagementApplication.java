713a27435daca5e28d7db74e582a5abcb275cd58
igorbaland <igor.balandinmx.de>
2022-07-01 14:10:42 +0200

    branch master - backend-spring-boot to repo

---
+++ backend-enterprise-management/src/main/java/com/bal/enterprisemanagement/EnterpriseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.bal.enterprisemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseManagementApplication.class, args);
+	}
+
+}
