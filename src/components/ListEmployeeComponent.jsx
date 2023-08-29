bf65abe79440119bfb9e8b498463c84ca5bcaf8f
Igor <igor.balandin@gmx.de>
2023-08-29 09:29:52 +0200

    added delete button. After excecute delete - update state

--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -28,6 +28,12 @@ class ListEmployeeComponent extends Component {
         this.props.navigate(`/add-employee/${id}`);
     }
 
+    deleteEmployee(id){
+        EmployeeService.deleteEmployee(id).then( res =>{
+            this.setState({employees: this.state.employees.filter(employee => employee.id !== id)});
+        });
+    }
+
     render() {
         return (
             <div>
@@ -58,6 +64,7 @@ class ListEmployeeComponent extends Component {
                                         <td>{employee.emailId}</td>
                                         <td>
                                             <button onClick={ () => this.editEmployee(employee.id)} className="btn btn-info">Update</button>
+                                            <button style={{marginLeft: "10px"}} onClick={ () => this.deleteEmployee(employee.id)} className="btn btn-danger">Delete</button>
                                         </td>
                                     </tr>
                                 )
@@ -66,7 +73,7 @@ class ListEmployeeComponent extends Component {
                     </table>
                 </div>
             </div>
-        );
+        )
     }
 }
 
